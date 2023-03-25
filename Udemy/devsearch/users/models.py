a72d7b914c1717e80ee5a0cf28020c987387ed61
Ahtesham-Uddowla <ahteshamuddowla@gmail.com>
2023-03-25 23:25:41 +0600

    Day 6

--- Udemy/devsearch/users/models.py
+++ Udemy/devsearch/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -23,7 +24,7 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
     
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -36,4 +37,6 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
+    
+
 

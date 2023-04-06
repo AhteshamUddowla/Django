2aad7450b7f04d765e3df8d7916b7ecd7f3ef31c
Ahtesham-Uddowla <ahteshamuddowla@gmail.com>
2023-04-06 22:56:43 +0600

    Day 12

--- Udemy/devsearch/users/models.py
+++ Udemy/devsearch/users/models.py
@@ -39,26 +39,25 @@ class Skill(models.Model):
     def __str__(self):
         return str(self.name)
 
-# @receiver(post_save, sender=Profile)
-# def createProfile(sender, instance, created, **kwagrs):
-#     print("Profile Saved")
-#     print("Instance:", instance)
-#     print("Created:", created)
-#     if created:
-#         user = instance
-#         profile = Profile.objects.create(
-#             user = user,
-#             username = user.username,
-#             email = user.email,
-#             name = user.first_name,
-#         )
+class Message(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(Profile, on_delete=models.SET_NULL,
+                                   null=True, blank=True, related_name='messages')
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, 
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.subject
+    
+    class Meta:
+        ordering = ['is_read', '-created']
 
 
-# def deleteUser(sender, instance, **kwagrs):
-#     print("Deleting user...")
-#     user = instance.user
-#     user.delete()
 
-# post_save.connect(createProfile, sender=User)
-# post_delete.connect(deleteUser, sender=Profile)
 
